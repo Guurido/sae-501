d9f0f09c592ff0c8cb4d1da21d94867517e7bfab
Yanis CHATELET <97635006+Guurido@users.noreply.github.com>
2023-11-22 15:34:01 +0100

    ajout possibilité erreur

--- src/scripts/back-end/form-validator.js
+++ src/scripts/back-end/form-validator.js
@@ -3,14 +3,14 @@ import validator from "validator"
 const form = document.querySelector("[data-form]")
  
 form.addEventListener("submit", (e) => {
-    // Prevent form to send data directly to back-end in order to catch
-    // form's data and use it
+    // Prevent form to send data directly to back-end in order to catch form's data and use it
     e.preventDefault()
  
     const formData = new FormData(e.target)
     const formValues = Object.fromEntries(formData)
  
     if(validator.isEmpty(formValues.title)) {
+        document.querySelector("[data-error-message='title']").classList.remove("hidden")
         // Display error message
         console.log("Erreur message pour titre")
         return;
