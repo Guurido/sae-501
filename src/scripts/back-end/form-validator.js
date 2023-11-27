4733d18ea125f5dc57a619cc6a91e3ac7dc88437
Yanis CHATELET <97635006+Guurido@users.noreply.github.com>
2023-11-27 14:11:04 +0100

    validator

--- src/scripts/back-end/form-validator.js
+++ src/scripts/back-end/form-validator.js
@@ -1,16 +1,73 @@
 import validator from "validator"
  
 const form = document.querySelector("[data-form]")
+const formName = e.target.dataset.form;
+const formValues = Object.fromEntries(formData)
  
+let hasErrors = false
+
+if(formName === "sae"){
+    hasErrors = SAEValidator(formValues)
+} else if (formName === "article"){
+    hasErrors = articleValidator(formValues)
+} else if (formName === "author"){
+    hasErrors = authorValidator(formValues)
+}
+
+if(!hasErrors){
+    e.target.submit()
+}
+
+
+const SAEValidator = (formValues) => {
+    let hasErrors = false
+
+    if (validator.isEmpty(formValues.title)){
+        document.querySelector("[data-error-message='title']").classList.remove("hidden")
+        hasErrors = true;
+    }
+
+    return hasErrors
+}
+
+const articleValidator = (formValues) => {
+    let hasErrors = false
+
+    if (validator.isEmpty(formValues.title)){
+        document.querySelector("[data-error-message='title']").classList.remove("hidden")
+        hasErrors = true;
+    }
+    if (validator.isEmpty(formValues.author)){
+        document.querySelector("[data-error-message='author']").classList.remove("hidden")
+        hasErrors = true;
+    }
+    if (validator.isEmpty(formValues.chapo)){
+        document.querySelector("[data-error-message='chapo']").classList.remove("hidden")
+        hasErrors = true;
+    }
+
+    return hasErrors
+}
+
+const authorValidator = (formValues) => {
+    let hasErrors = false
+
+    if (validator.isEmpty(formValues.title)){
+        document.querySelector("[data-error-message='title']").classList.remove("hidden")
+        hasErrors = true;
+    }
+
+    return hasErrors
+}
+
 form.addEventListener("submit", (e) => {
-    // Prevent form to send data directly to back-end in order to catch form's data and use it
+    // Prevent form to send data directly to back-end in order to catch
+    // form's data and use it
     e.preventDefault()
  
     const formData = new FormData(e.target)
-    const formValues = Object.fromEntries(formData)
  
     if(validator.isEmpty(formValues.title)) {
-        document.querySelector("[data-error-message='title']").classList.remove("hidden")
         // Display error message
         console.log("Erreur message pour titre")
         return;
