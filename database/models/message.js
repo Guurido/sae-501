8b9417cf07589358fd0dc1c9460e396872dc0618
Yanis CHATELET <97635006+Guurido@users.noreply.github.com>
2023-11-30 13:51:55 +0100

    ajout model

---
+++ database/models/message.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+
+const messageSchema = new Schema({
+    lastname: {
+        type: String,
+        required: [true, "Nom de famille requis"]
+    },
+    firstname: {
+        type: String,
+        required: [true, "Prénom requis"]
+    },
+    email: {
+        type: String,
+        required: [true, "E-mail requis"],
+        validate: [validator.isEmail, "E-mail non valide"]
+    },
+    content: {
+        type: String,
+        required: [true, "Message requis"]
+    },
+    type: {
+        type: String,
+        enum: ["non_précise", "autre", "etudiant", "parent"],
+        default: ["non_précise"]
+    }
+})
+
+export default mongoose.model("Message", messageSchema);
