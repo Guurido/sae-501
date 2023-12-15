28b9f3c5f0cac7a727ab6e986aa735b9c5687a94
Samad Abdul <absamad938@gmail.com>
2023-12-15 16:32:45 +0100

    ajout de la page 404

--- server/front-end-router.js
+++ server/front-end-router.js
@@ -52,32 +52,12 @@ router.get("/", async (req, res) => {
 
 // "(.html)?" makes ".html" optional
 router.get("/a-propos(.html)?", async (_req, res) => {
-    let options = {
-        method: "GET",
-        url: `${res.locals.base_url}/api/saes?per_page=9`,
-    };
-
-    let result = null;
-    try {
-        result = await axios(options);
-    } catch (e) {}
-
     res.render("pages/front-end/a-propos.njk", {
-        list_saes: result.data,
+        //list_saes: result.data,
     });
 });
 
 router.get("/lieux-de-vie(.html)?", async (_req, res) => {
-    let options = {
-        method: "GET",
-        url: `${res.locals.base_url}/api/saes?per_page=9`,
-    };
-
-    let result = null;
-    try {
-        result = await axios(options);
-    } catch (e) {}
-
     res.render("pages/front-end/lieux-de-vie.njk", {
         // list_saes: result.data,
         // Mettre contenu lieu de vie à la place de list sae
@@ -85,11 +65,6 @@ router.get("/lieux-de-vie(.html)?", async (_req, res) => {
 });
 
 router.get("/author(.html)?", async (_req, res) => {
-    let result = null;
-    try {
-        result = await axios(options);
-    } catch (e) {}
-
     res.render("pages/front-end/author.njk", {
         // list_medias: result.data,
         // Mettre contenu media à la place de list sae
@@ -97,37 +72,21 @@ router.get("/author(.html)?", async (_req, res) => {
 });
 
 router.get("/a-propos(.html)?", async (_req, res) => {
-    let options = {
-        method: "GET",
-        url: `${res.locals.base_url}/api/saes?per_page=9`,
-    };
-
-    let result = null;
-    try {
-        result = await axios(options);
-    } catch (e) {}
-
     res.render("pages/front-end/a-propos.njk", {
-        list_saes: result.data,
+        //list_saes: result.data,
     });
 });
 
 
 router.get("/contact(.html)?", async (_req, res) => {
-    let options = {
-        method: "GET",
-        url: `${res.locals.base_url}/api/saes?per_page=9`,
-    };
-
-    let result = null;
-    try {
-        result = await axios(options);
-    } catch (e) {}
-
     res.render("pages/front-end/contact.njk", {
         // list_contact: result.data,
         // Mettre contenu contact à la place de list sae
     });
+});
+
+router.use((_req, res) => {
+    res.status(404).render("pages/front-end/404.njk");
 });
 
 export default router;
