8b4092c49e1f7a844171f20f7bedc861a6f000b0
Yanis CHATELET <97635006+Guurido@users.noreply.github.com>
2024-01-24 14:02:22 +0100

    correction bug contact

--- server/front-end-router.js
+++ server/front-end-router.js
@@ -93,7 +93,7 @@ router.get("/sur-les-medias(.html)?", async (_req, res) => {
 
 router.get("/contact(.html)?", async (_req, res) => {
     res.render("pages/front-end/contact.njk", {
-        list_messages: result.data,
+        // list_messages: result.data,
     });
 });
 
